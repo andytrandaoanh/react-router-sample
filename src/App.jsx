955439363c973bc4eecd44bbf1d0ce51f03bcbcb
andytrandaoanh <andytrandaoanh@gmail.com>
2019-06-14 07:19:10 +0700

    first react-route 2019 example

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Swith } from 'react-dom';
+import './App.css';
+
+function App () {
+	return (
+
+		<div className = "App">
+			<header>
+				<ul>
+					<li>Home</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</header>
+			<main>
+				<h1>Welcome to React Router</h1>
+			</main>
+		</div>
+	)
+}
+
+export default App;
+
+
+<--! https://learnwebtutorials.com/tutorial-on-react-router-5 -->

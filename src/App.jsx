aa1e4ebf6797470c3243dcb1293bf9ea5de1cef9
Andy Tran <andytrandaoanh@gmail.com>
2019-06-14 17:14:47 +0700

    add and test router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
 import React from 'react';
-import { BrowserRouter, Route, Swith } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './App.css';
+
+const HomeComponent = () => {
+	return (
+		<div><h1>Home Component</h1></div>
+	)
+}
+
+const AboutComponent = () => {
+	return (
+		<div><h1>About Component</h1></div>
+	)
+}
+
+const ContactComponent = () => {
+	return (
+		<div><h1>Contact Component</h1></div>
+	)
+}
+
 
 function App () {
 	return (
 
 		<div className = "App">
+			
+		<Router>
 			<header>
 				<ul>
-					<li>Home</li>
-					<li>About</li>
-					<li>Contact</li>
+					<li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+					<li><NavLink to="/about" exact activeClassNaem = "active">About</NavLink></li>
+					<li><NavLink to="/about/contact" activeClassName = "active">Contact</NavLink></li>
 				</ul>
 			</header>
 			<main>
-				<h1>Welcome to React Router</h1>
+				
+					<Switch>
+						<Route path="/" exact component={HomeComponent} />
+						<Route path="/about/contact" component={ContactComponent} />
+						<Route path="/about" component={AboutComponent} />
+					</Switch>
+				
 			</main>
+		</Router>
 		</div>
 	)
 }
@@ -23,4 +52,3 @@ function App () {
 export default App;
 
 
-<--! https://learnwebtutorials.com/tutorial-on-react-router-5 -->
